bdf30a46b4467c80fe57f218c2d70311abd50138
PC 6 <>
2024-06-13 17:19:23 -0600

    actualice y elimine registros de la base de datos

--- comandos.sql
+++ comandos.sql
@@ -35,4 +35,38 @@ SHOW TABLES;
 /* ver los registros de una tabla */
 SELECT * FROM productos;
 
+/* detalla los campos de una tabla */
+
+DESCRIBE productos; 
+
 /* ---------------------------------- FIN READ -------------- */
+
+/* ---------------------------------- UPDATE -------------- */
+
+ALTER TABLE productos ADD COLUMN tiene_oferta TINYINT(1) DEFAULT 0;
+
+ALTER TABLE productos ADD COLUMN peso FLOAT;
+
+ALTER TABLE productos ADD COLUMN tamanio FLOAT NOT NULl;
+
+ALTER TABLE productos ADD COLUMN unidad VARCHAR(3) NOT NULl;
+
+
+UPDATE productos
+SET peso = 1 WHERE id = 2;
+
+UPDATE productos
+SET peso = 2 WHERE id = 4;
+
+UPDATE productos
+SET unidad = "kg" WHERE id = 3;
+
+/* ---------------------------------- FIN UPDATE -------------- */
+
+/* ---------------------------------- DELETE -------------- */
+
+DELETE FROM productos WHERE id = 3;
+
+DELETE FROM productos WHERE id = 1;
+
+/* ---------------------------------- FIN DELETE -------------- */
